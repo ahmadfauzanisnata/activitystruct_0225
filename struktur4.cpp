e1414cbe6f99e826f218f897d8b0693431998908
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-02 17:13:58 +0700

    menambahkan arr i setiap variabel mhs

--- struktur4.cpp
+++ struktur4.cpp
@@ -29,18 +29,20 @@ int main(){
         cout << "umur Mahasiswa : ";
         cin >> mhs[i].umur;
         cin.ignore(1,'\n');
+        cout << endl;
 
 
-        cout << "Umur mahasiswa : ";
-        cin >> mhs[i].umur;
+       
     }
     for (int i=0;i<3;i++){
         cout << endl;
+        cout << "Data Mahasiswa ke-"<< (i+1)<<":"<<endl;
         cout <<"\n Nim : "<<mhs[i].nim;
         cout <<"\n Nama : "<<mhs[i].nama;
         cout <<"\n Alamat : ";
         cout << " \n \t Desa : "<< mhs[i].alamat.desa;
         cout << " \n \t Kota : "<< mhs[i].alamat.desa;
         cout <<"\n Umur : "<<mhs[i].umur;
+        cout<<endl;
     }
 }
