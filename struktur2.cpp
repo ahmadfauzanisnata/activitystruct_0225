6318c237c2e16e18de6373aafcb826d70d8d6421
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-02 16:06:25 +0700

    membuat tab baru pada alamat

--- struktur2.cpp
+++ struktur2.cpp
@@ -11,3 +11,27 @@ struct mahasiswa {
     DetailAlamat alamat;
     string umur;
 };
+
+int main(){
+    mahasiswa mhs;
+    cout << "Nomor mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "Alamat mahasiswa : ";
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout <<"\n Nim : "<<mhs.nim;
+    cout <<"\n Nama : "<<mhs.nama;
+    cout <<"\n Alamat : "<<mhs.alamat;
+    cout <<"\n Umur : "<<mhs.umur;
+
+}
